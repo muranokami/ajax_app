cceb1e51dfd795fb06d7a865b4db162ea1346b12
muranokami <pikupiku68574.dj@gmail.com>
2020-12-05 19:54:18 +0900

    AjaxAppの準備

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,8 @@
 ActiveRecord::Schema.define(version: 2020_11_16_052711) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.text "memo"
+    t.text "content"
+    t.boolean "checked"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
